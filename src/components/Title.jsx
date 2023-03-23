423d5aeb3576cfb084949bf1b1e53f02f08cef8c
ibsenc <ibsen.camille@gmail.com>
2023-03-23 10:10:54 -0700

    Added back button on game page and fixed play button

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,13 +1,22 @@
 import "./Title.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 export default function Title(props) {
-  const { headerHeight, textSize } = props;
+  const { headerHeight, textSize, hasBackButton } = props;
 
   return (
     <div className='title-header' style={{ height: headerHeight }}>
+      {hasBackButton && (
+        <Link className='back-button' to={"/"}>
+          <FontAwesomeIcon icon={faArrowLeftLong} />
+        </Link>
+      )}
       <div className='title' style={{ fontSize: textSize }}>
         Wordle
       </div>
+      {hasBackButton && <div className='space-holder'></div>}
     </div>
   );
 }
