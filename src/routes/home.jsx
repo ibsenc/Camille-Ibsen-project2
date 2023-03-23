952b1c2411e22aa40bfea83bf634e3439b6475aa
ibsenc <ibsen.camille@gmail.com>
2023-03-23 14:01:58 -0700

    Removes styling in homepage

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -12,50 +12,33 @@ export default function Home() {
   };
 
   return (
-    <div style={{ backgroundColor: "whitesmoke" }}>
+    <div className='background-container'>
       <Title headerHeight='180px' textSize='60px' hasRules={true} />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
+      <div className='gif-container'>
         <img
+          className='gameplay-gif'
           src='src/resources/gameplay.gif'
           width={"350px"}
-          style={{
-            borderStyle: "solid",
-            borderColor: "grey",
-          }}
         />
       </div>
-      <div
-        style={{
-          display: "flex",
-          margin: "auto",
-          justifyContent: "center",
-          flexDirection: "column",
-        }}
-      >
-        <div style={{ marginBottom: "40px" }}>
-          <PlayButton
-            func={function () {
-              location.href = `/game/${hardMode ? "hard" : "normal"}`;
-            }}
-            text='Start Game'
-            isStartButton={true}
-          />
-          <div className='switch-outer-container'>
-            <div className={`switch-label ${hardMode ? "" : "greyed-out"}`}>
-              Hard Mode
-            </div>
-            <div className='switch-container'>
-              <Switch
-                checked={hardMode}
-                onChange={toggleHardMode}
-                inputProps={{ "aria-label": "controlled" }}
-              />
-            </div>
+      <div className='play-button-and-toggle-container'>
+        <PlayButton
+          func={function () {
+            location.href = `/game/${hardMode ? "hard" : "normal"}`;
+          }}
+          text='Start Game'
+          isStartButton={true}
+        />
+        <div className='switch-outer-container'>
+          <div className={`switch-label ${hardMode ? "" : "greyed-out"}`}>
+            Hard Mode
+          </div>
+          <div className='switch-container'>
+            <Switch
+              checked={hardMode}
+              onChange={toggleHardMode}
+              inputProps={{ "aria-label": "controlled" }}
+            />
           </div>
         </div>
       </div>
